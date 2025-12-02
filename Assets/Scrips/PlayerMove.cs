c92422bf5cd4c03ad8240a03fb58b97ae8015c12
G100wasd <3882517387@qq.com>
2025-12-02 19:34:58 +0800

    source
    部分素材

--- Assets/Scrips/PlayerMove.cs
+++ Assets/Scrips/PlayerMove.cs
@@ -16,8 +16,7 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(IsGround.isGround);
-        if (IsGround.isGround)
+        if (IsGround.isGround || isMovePlane.isPlane)
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
